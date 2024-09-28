d3faa7e6cdc79a12a3666b1afb540f966fc657d9
Daniel Diego Horcajuelo <dadiego91@hotmail.com>
2024-09-28 12:34:36 +0200

    adds new utility functions and updates notebook introduccion-spark

---
+++ blackops/utils/catalog.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession, DataFrame
+import pyspark.sql.functions as f
+
+
+def get_detailed_tables_info(spark: SparkSession) -> DataFrame:
+    return (
+        spark.sql("show table extended like '*'")
+        .withColumn("information", f.explode(f.split("information", "\n")))
+        .select(
+            "*",
+            f.explode(
+                f.create_map(
+                    f.regexp_extract("information", r"^([A-Z][\w\s]+?): (.+)$", 1),
+                    f.regexp_extract("information", r"^([A-Z][\w\s]+?): (.+)$", 2),
+                )
+            ).alias("key", "value"),
+        )
+        .groupBy("namespace", "tableName")
+        .pivot("key")
+        .agg(f.first("value"))
+        .drop("Schema")
+        .orderBy("namespace", "tableName")
+    )
