56b38a09fb68ccc37641a0949f8f709c70850fac
Daniel Diego Horcajuelo <dadiego91@hotmail.com>
2024-09-29 14:11:28 +0200

    adds spark session initializer in utils and psutil as a dependency

--- blackops/utils/catalog.py
+++ blackops/utils/catalog.py
@@ -1,5 +1,32 @@
+import psutil
 import pyspark.sql.functions as f
 from pyspark.sql import DataFrame, SparkSession
+
+
+def start_spark_session() -> SparkSession:
+    """
+    Initializes a SparkSession locally with Delta catalog enabled, using half of the total RAM available in
+    the system.
+    """
+    driver_memory = round(psutil.virtual_memory().total / 1024**3 / 2)
+    spark = (
+        SparkSession.Builder()
+        .master("local[*]")
+        .config(
+            map={
+                "spark.driver.memory": f"{driver_memory}g",
+                "spark.jars.packages": "io.delta:delta-spark_2.12:3.2.0",
+                "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+                "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+                "spark.databricks.delta.retentionDurationCheck.enabled": "false",
+                "spark.sql.catalogImplementation": "hive",
+                "spark.sql.repl.eagerEval.enabled": "true",
+                "spark.sql.repl.eagerEval.truncate": "100",
+            }
+        )
+        .getOrCreate()
+    )
+    return spark
 
 
 def get_detailed_tables_info(spark: SparkSession) -> DataFrame:
